db0f94f2561c1a9891adc8016d861044aaaf4c86
sunxiuguo <sunxiuguo@guazi.com>
2019-02-04 00:12:55 +0800

    第一题添加另一种解法

--- 剑指offer/1-二维数组中的查找.js
+++ 剑指offer/1-二维数组中的查找.js
@@ -4,6 +4,7 @@
 // 每一列都按照从上到下递增的顺序排序。
 // 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
+
 function Find(target, array){
     // write code here
     let rowLength = array[0].length;
@@ -19,6 +20,36 @@ function Find(target, array){
             if (number === target) {
                 return true;
             }
+        }
+    }
+    return false;
+}
+
+function Find1(target, array)
+{
+    // write code here
+    let colLength = array[0].length;
+    let rowLength = array.length;
+    if (array[rowLength-1][colLength-1] < target) {
+        return false;
+    }
+    if (target < array[0][0]) {
+        return false;
+    }
+
+    // 从左下角开始查找
+    let i= rowLength - 1;
+    let j = 0;
+    while (i >= 0 && j < colLength) {
+        let currentNum = array[i][j];
+        if (currentNum === target) {
+            return true;
+        } else if (currentNum < target) {
+            // 如果左下角的值小于target, 则向右查找
+            j++;
+        } else if (currentNum > target) {
+            // 如果左下角的值大于target, 则向上查找            
+            i--;
         }
     }
     return false;
