4d2f97818556266e3a181aa4da5fec3e411f7f59
Sun Xiu Guo <sunxg@fenbi.com>
2019-03-22 19:53:19 +0800

    leetcode 1

--- LeetCode/1-两数之和.js
+++ LeetCode/1-两数之和.js
@@ -14,7 +14,20 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
-    
+let twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i],i);
+    }
+    for (let i = 0; i< nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement) && map.get(complement) !== i) {
+            return [i, map.get(complement)]
+        }
+    }
 }
+
+let nums = [2, 7, 11,15];
+let target = 9;
+let result = twoSum(nums, target);
+console.log(result)
