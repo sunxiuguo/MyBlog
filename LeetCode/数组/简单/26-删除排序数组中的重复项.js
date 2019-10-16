eabc77fe1d667c3060567629143f27d43955231a
Sun Xiu Guo <sunxg@fenbi.com>
2019-10-16 18:53:32 +0800

    ADD: 26-双指针

--- LeetCode/数组/简单/26-删除排序数组中的重复项.js
+++ LeetCode/数组/简单/26-删除排序数组中的重复项.js
@@ -53,5 +53,38 @@ const removeDuplicates1 = function(nums) {
     return length - count;
 };
 
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates2 = function(nums) {
+    let current = nums[0];
+    for (let i = 1; i < nums.length;) {
+        if (nums[i] === current) {
+            nums.splice(i, 1);
+        } else {
+            current = nums[i++];
+        }
+    }
+    return nums.length;
+};
+
+/**
+ * 双指针
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates3 = function(nums) {
+    let j = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i-1]) {
+            j++;
+            nums[j] = nums[i];
+        }
+    }
+    return j+1;
+};
+
 const res = removeDuplicates1([1,1,2]);
 console.log(res); 
